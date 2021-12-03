9364d63d9ce84aa4f1fa5f1bbe2809435dc1e6bb
Ali Emrah PEKESEN <aliemrahpekesen@gmail.com>
2021-12-03 13:11:20 +0300

    clean up commit

--- src/main/java/com/aep/training/repository/OpLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aep.training.repository;
-
-import com.aep.training.domain.entity.OpLog;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OpLogRepository extends CrudRepository<OpLog,Long> {
-}
